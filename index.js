e7dc125c9496329887f6b374a4d98c2694d31a3f
Andrea Soto <sotoandrea585@gmail.com>
2026-04-01 21:04:06 -0400

    Added views and changes to index.js

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//app.use(express.json());
 
 app.use(session({
     secret: 'esta es la clave de encriptación de la sesión y puede ser cualquier texto',
     resave: false,
     saveUninitialized: false
 }));
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.session.user || null;
+    res.locals.userId = req.session.user ? req.session.user.id : null;
+    res.locals.userEmail = req.session.user ? req.session.user.email : null;
+    next();
+});
 
 require('./controllers')(app, db);
 
